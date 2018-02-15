2521149032d90ce9743e3ade80c6ec575ee8f79f
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-02-15 10:44:41 -0400

    [ADD] classe aluno

--- gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -1,13 +1,16 @@
 package br.com.alura;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
+	private Set<Aluno> alunos = new HashSet<>();
 
 	public Curso(String nome, String instrutor) {
 		super();
@@ -38,7 +41,15 @@ public class Curso {
 
 	@Override
 	public String toString() {
-		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + ", aulas: " + this.aulas + "]";
+		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + ", aulas: " + this.aulas + "], [alunos: " + alunos + "]";
+	}
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+	}
+	
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
 	}
 
 }
